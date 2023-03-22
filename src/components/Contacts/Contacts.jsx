35554c65c11990683a6d165cea59a96fc5938df8
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2023-03-22 14:02:50 +0200

    prettier

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,31 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Contacts.module.css';
 
-
-export default function ContactList ({contacts, deleteContact}) {
-    return (
-        <ol className={css.ContactList}>
-            {contacts.map(({id, name, number}) => {
-                return (
-                    <li className={css.ContactList_item} key={id}>
-                        <p>
-                        {name}: {number}
-                        </p>
-                        <button className={css.ContactList_btn}
-                        onClick={() => deleteContact(id)} 
-                        type="button">
-                        Delete 
-                        </button>
-                    </li>
-                )
-            })}
-        </ol>
-    )
+export default function ContactList({ contacts, deleteContact }) {
+  return (
+    <ol className={css.ContactList}>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li className={css.ContactList_item} key={id}>
+            <p>
+              {name}: {number}
+            </p>
+            <button
+              className={css.ContactList_btn}
+              onClick={() => deleteContact(id)}
+              type="button"
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ol>
+  );
 }
 
 ContactList.propTypes = {
-    id: PropTypes.number,
-    name: PropTypes.string,
-    number: PropTypes.number,
-    onRemoveContact: PropTypes.func,
-  };
+  id: PropTypes.number,
+  name: PropTypes.string,
+  number: PropTypes.number,
+  onRemoveContact: PropTypes.func,
+};
